99f4217dc07a57fc1ccce439f856c26050f2bcd8
Jian Li <hbjoylee@gmail.com>
2022-10-13 12:49:12 +1100

    add clean-images.sh

---
+++ clean-images.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ -z "${1}" ]; then
+      echo "Need pass tag as argument, e.g ./clean-images.sh 1.0.0"
+      exit 1
+  else
+        version="${1}"
+fi
+docker rmi gennyproject/genny-website:${version}
